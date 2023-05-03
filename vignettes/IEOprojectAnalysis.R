0a02b92f6708988bfbdd6299ea0abf8fe55b0697
Nikola <n.panajotovic@outlook.com>
2023-05-03 13:27:43 +0000

    Wed May  3 01:27:43 PM UTC 2023

--- vignettes/IEOprojectAnalysis.R
+++ vignettes/IEOprojectAnalysis.R
@@ -62,8 +62,16 @@ names(colData(se))[35] <- "protocol"
 
 # creating factors
 colData(se)$idFac <- factor(colData(se)$id)
-colData(se)$lacStageFac <- factor(colData(se)$lacStage)
-colData(se)$protocolFac <- factor(colData(se)$protocol)
+# colData(se)$lacStageFac <- factor(colData(se)$lacStage)
+colData(se)$lacStageFac <- factor(colData(se)$lacStage,
+                                  levels = c("Colostrum", "Transitional", "Mature"),
+                                  labels = c(1, 2, 3))
+
+#colData(se)$protocolFac <- factor(colData(se)$protocol)
+colData(se)$protocolFac <- factor(colData(se)$protocol,
+                                  levels = c("Soft spin, Unwashed", "Hard spin, Unwashed", "Hard spin, Washed once", "Hard spin, Washed twice"),
+                                  labels = c(1, 2, 3, 4 ))
+
 
 ## ----pheno, echo=FALSE, message=FALSE-----------------------------------------
 tmpdf <- data.frame("Patient"=colData(se)$idFac,
@@ -72,4 +80,76 @@ tmpdf <- data.frame("Patient"=colData(se)$idFac,
                     check.names=FALSE)
 ktab <- kable(tmpdf, caption="Phenotypic variables.")
 kable_styling(ktab, position="center")
+
+## -----------------------------------------------------------------------------
+par(mar=c(7, 5, 2, 2))
+ord <- order(dge$sample$lib.size/1e6)
+ordmreads <- dge$sample$lib.size[ord]/1e6
+names(ordmreads) <- colnames(se)[ord]
+bp <- barplot(ordmreads, las=1, ylab="Millions of reads",
+              xlab="", col=c("blue", "green", "red")[colData(se)$lacStageFac[ord]], las=2, ylim = c(0, 50))
+legend("topleft", c("Colostrum", "Transitional", "Mature"), fill=c("blue", "green", "red"), inset=0.01)
+
+## -----------------------------------------------------------------------------
+par(mar=c(7, 5, 2, 2))
+ord <- order(dge$sample$lib.size/1e6)
+ordmreads <- dge$sample$lib.size[ord]/1e6
+names(ordmreads) <- colnames(se)[ord]
+bp <- barplot(ordmreads, las=1, ylab="Millions of reads",
+              xlab="", col=c("blue", "green", "red", "orange")[colData(se)$protocolFac[ord]], las=2, ylim = c(0, 60))
+legend("topleft", c("Soft spin, Unwashed", "Hard spin, Unwashed", "Hard spin, Washed once", "Hard spin, Washed twice"), 
+       fill=c("blue", "green", "red", "orange"), inset=0.01)
+
+## ----distRawExp, echo=FALSE, fig.height=5, fig.width=5, out.width="600px", fig.cap="Non-parametric density distribution of expression profiles per sample.", message=FALSE----
+library(geneplotter)
+par(mar=c(4, 5, 1, 1))
+lst <- as.list(as.data.frame(assays(se)$logCPM))
+multidensity(lst, xlab="log 2 CPM", legend=NULL,
+             main="", las=1)
+
+## -----------------------------------------------------------------------------
+par(mar=c(7, 5, 2, 2))
+boxplot(assays(se)$logCPM, col="gray", ylab=expression(log[2] * "CPM"),
+cex.axis=1.2, cex.lab=1.5, las=2)
+
+
+## -----------------------------------------------------------------------------
+# Create logical mask excluding column containing "SRR801705" in the name
+mask <- !grepl("SRR801705", colnames(se))
+
+# Subset the data frame using the mask
+se_sample_filtered <- se[, mask]
+
+
+## ----exprdist, echo=FALSE, out.width="600px", fig.cap="Distribution of average expression level per gene."----
+avgexp <- rowMeans(assays(se_sample_filtered)$logCPM)
+hist(avgexp, xlab="log2 CPM", main="", las=1, ylim=c(0,7000))
+
+## -----------------------------------------------------------------------------
+mask <- filterByExpr(dge, group=se$samplegroup)
+se.filt <- se[mask, ]
+dim(se.filt)
+dge.filt <- dge[mask, ]
+dim(dge.filt)
+
+## -----------------------------------------------------------------------------
+dge.filt <- calcNormFactors(dge.filt)
+
+## -----------------------------------------------------------------------------
+assays(se.filt)$logCPM <- cpm(dge.filt, log=TRUE,
+                              normalized.lib.sizes=TRUE)
+
+## ----maPlots, fig.height=18, fig.width=10, dpi=100, echo=FALSE, fig.cap="MA-plots of filtered and normalized expression values."----
+par(mfrow=c(5, 4), mar=c(4, 5, 3, 1))
+for (i in 1:ncol(se.filt)) {
+  A <- rowMeans(assays(se.filt)$logCPM)
+  M <- assays(se.filt)$logCPM[, i] - A
+  smoothScatter(A, M, main=colnames(se.filt)[i], las=1)
+  abline(h=0, col="blue", lwd=2)
+  lo <- lowess(M ~ A)
+  lines(lo$x, lo$y, col="red", lwd=2)
+}
+
+## -----------------------------------------------------------------------------
+table(se.filt$cell_line, se.filt$treatment)
 
