3fe622a223ffc03ae6fcebe33ea9136238bc6f7f
Nikola <n.panajotovic@outlook.com>
2023-06-08 10:23:42 +0000

    Thu Jun  8 10:23:42 AM UTC 2023

--- doc/IEOprojectAnalysis.R
+++ doc/IEOprojectAnalysis.R
@@ -535,53 +535,6 @@ KEGGresults <- summary(KEGGhgOver)
 KEGGresults
 
 ## -----------------------------------------------------------------------------
-getKEGGName <- function(input) {
-  # Construct the KEGG ID
-  kegg_id <- paste0("hsa", input)
-  
-  tryCatch(
-    {
-      # Retrieve KEGG information
-      rezKEGG <- keggGet(kegg_id)
-      name <- rezKEGG[[1]]$NAME
-      name <- gsub(" - Homo sapiens \\(human\\)", "", name)
-      return(name)
-  
-    },
-    error = function(err) {
-      # Code to execute if an error occurs
-      name <- "no info"
-      return(name)
-    }
-  )
-}
-
-## -----------------------------------------------------------------------------
-getKEGGClass <- function(input) {
-  
-  # Construct the KEGG ID
-  kegg_id <- paste0("hsa", input)
-  
-  tryCatch(
-    {
-      # Retrieve KEGG information
-      rezKEGG <- keggGet(kegg_id)
-      class_name <- rezKEGG[[1]]$CLASS
-      if (is.null(class_name)) {
-        class_name <- "no info"
-      }
-      return(class_name)
-  
-    },
-    error = function(err) {
-      # Code to execute if an error occurs
-      class_name <- "no info"
-      return(class_name)
-    }
-  )
-}
-
-## -----------------------------------------------------------------------------
 source("../R/custom_functions.R")
 
 names_names <- sapply(KEGGresults$KEGGID, getKEGGName)
